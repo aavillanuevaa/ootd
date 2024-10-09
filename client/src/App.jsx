120164abf68ffdcfc91d8d0799de385c4a36c054
aavillanuevaa <angelovill0908@gmail.com>
2024-10-09 16:33:03 -0400

    backend

---
+++ client/src/App.jsx
@@ -0,0 +1,39 @@
+import { React, useEffect } from 'react';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Fits } from './pages/fits/Page';
+import { Pieces } from './pages/pieces/Page';
+import { Daily } from './pages/daily/Page';
+import { Layout } from './Layout';
+import axios from "axios";
+
+
+function App() {
+  
+
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:8080/api");
+   
+    console.log(response.data.fruits);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  return (
+    <>  
+      <Router>
+        <Routes>
+          <Route element={<Layout/>}>
+            <Route path="/" element={<Fits/>}/>
+            <Route path="/pieces" element={<Pieces/>}/>
+            <Route path="/daily" element={<Daily/>}/>
+          </Route>
+        </Routes>
+      </Router>  
+    </>
+      
+  );
+}
+
+export default App
---
+++ client/src/components/Button.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+
+export const Button = (props) => {
+    return (
+      <>
+        <div className="relative text-subtext1 hover:text-black cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-200 before:absolute before:bg-black before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-200 after:absolute after:bg-black after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">
+            <span><Link to={props.page}>{props.name}</Link></span>
+        </div>
+      </>
+    );
+  };
+  
+
+
+ 
+
+
+  
