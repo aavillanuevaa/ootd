1001456f3640fc9b924b94868f183f69bced43aa
aavillanuevaa <angelovill0908@gmail.com>
2024-10-11 17:57:23 -0400

    node_modules

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,11 +8,9 @@ import axios from "axios";
 
 
 function App() {
-  
 
   const fetchAPI = async () => {
     const response = await axios.get("http://localhost:8080/api");
-   
     console.log(response.data.fruits);
   };
 
